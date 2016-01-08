244eb0853c82ccd638448e53f7988057612554c7
Christophe Gragnic <christophegragnic@gmail.com>
2016-01-08 19:08:18 +0100

    Empty project.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject microalg "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :aot [microalg.core]
+  :main microalg.core)
---
+++ src/microalg/core.clj
@@ -0,0 +1,6 @@
+(ns microalg.core
+  (:gen-class))
+
+(defn -main
+  []
+  (println "Hello, World!"))
